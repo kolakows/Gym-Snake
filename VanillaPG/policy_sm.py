d684b2f31b52f6a7da4cfa8914f404c3fb352984
kolakows <kolakowski.igor@gmail.com>
2020-12-21 17:49:17 +0100

    move policy varants

--- VanillaPG/policy_sm.py
+++
@@ -1,7 +0,0 @@
-
-from VanillaPG.pg_policy import PGPolicy
-from VanillaPG.pg_network import PGNetwork
-
-class PolicySM(PGPolicy):
-    def __init__(self, obs_size, action_size, parameters):
-        super().__init__(obs_size, action_size, PGNetwork, parameters)
--- VanillaPG/policy_variants.py
+++ VanillaPG/policy_variants.py
@@ -2,6 +2,10 @@
 from VanillaPG.pg_policy import PGPolicy
 from VanillaPG.pg_network import PGNetwork
 
+class PolicySM(PGPolicy):
+    def __init__(self, obs_size, action_size, parameters):
+        super().__init__(obs_size, action_size, PGNetwork, parameters)
+
 class PolicyRewards(PGPolicy):
     def __init__(self, obs_size, action_size, parameters):
         super().__init__(obs_size, action_size, PGNetwork, parameters)
